37dd5e33e61f0d11c478ef9b751b1163f72aafc9
gpizzutoa <guero.pizzutoa@gmail.com>
2023-10-12 21:07:18 +0200

    Update Project_Code.py

--- Project_Code.py
+++ Project_Code.py
@@ -57,76 +57,76 @@ plt.show()
 
 #QUERY:
 #While considering the largest component of your network. Depending on what you prefer/seems more relevant in your graph,  
-#a) Compute Average distance and Diameter,
-#b) Improve the function computing the inverse distance function.
 #a') Compute Average clustering and Transitivity number,
 #b') Implement a function computing the transitivity using basic function of networkx
 
-#START CODE HERE:
+# Load nodes and edges from CSV files
+nodes_df = pd.read_csv('nodes.csv')
+edges_df = pd.read_csv('edges.csv')
+edges_df.columns = edges_df.columns.str.strip()  # Trim column names
 
-# Extract the largest component of the graph 'G'
-largest_component = max(nx.connected_components(G), key=len)
-G_largest = G.subgraph(largest_component)
+# Create the graph
+G = nx.from_pandas_edgelist(edges_df, source='# source', target='target')
 
-# OPTION 1: Average distance and Diameter calculations
+# Extract the largest connected component
+largest_cc = max(nx.connected_components(G), key=len)
+G_main = G.subgraph(largest_cc)
 
-# Compute the Average distance of the largest component
-avg_distance = nx.average_shortest_path_length(G_largest)
-
-# Compute the Diameter of the largest component
-diameter = nx.diameter(G_largest)
-
-# Function to compute the inverse distance between two nodes
-def inverse_distance(graph, node1, node2):
-    
-    #Compute the inverse distance between two nodes.
-    #If nodes are disconnected, return 0.
-    
-    try:
-        distance = nx.shortest_path_length(graph, node1, node2)
-        return 1 / distance if distance != 0 else 0
-    except nx.NetworkXNoPath:
+# Custom function to compute clustering of a node
+def clustering(G, node):
+    k = G.degree[node]
+    if k == 0 or k == 1:
         return 0
+    else:
+        List_nodes = [s for s in G.nodes()]
+        i = List_nodes.index(node)
+        A = nx.adjacency_matrix(G)
+        A3 = A**3
+        triangle = A3[i, i] / 2
+        den = k * (k - 1) / 2
+        return triangle / den
 
-# OPTION 2: Average clustering coefficient and Transitivity calculations
+# Custom function to compute average clustering of the graph
+def average_clustering(G):
+    N = G.number_of_nodes()
+    Temp_sum = sum(clustering(G, i) for i in G.nodes()) #Sam
+    return Temp_sum / N
 
-# Compute the Average clustering coefficient of the graph
-avg_clustering = nx.average_clustering(G)
+# Custom function to compute transitivity using basic functions of NetworkX
+def custom_transitivity(G):
+    triangles = sum(nx.triangles(G, node) for node in G.nodes()) / 3
+    triplets = sum(d * (d - 1) for n, d in G.degree()) / 2
+    return triangles / triplets if triplets != 0 else 0
 
-# Compute the Transitivity number of the graph
-transitivity = nx.transitivity(G)
+# Compute average clustering using NetworkX's built-in function
+avg_clustering_nx = nx.average_clustering(G_main)
 
-# Function to compute transitivity using basic functions of NetworkX
-def compute_transitivity(graph):
-    
-    #Compute the transitivity of a graph using basic functions of NetworkX.
-    
-    triangles = sum(nx.triangles(graph).values()) / 3  # Each triangle counted thrice
-    triples = sum(len(list(nx.all_neighbors(graph, node))) * (len(list(nx.all_neighbors(graph, node))) - 1) for node in graph) / 2
-    
-    return 3 * triangles / triples if triples != 0 else 0
+# Compute average clustering using custom function
+avg_clustering_custom = average_clustering(G_main)
 
-# Compute transitivity using the implemented function
-computed_transitivity = compute_transitivity(G)
+# Compute transitivity using NetworkX's built-in function
+transitivity_nx = nx.transitivity(G_main)
 
-print("OPTION A:")
-print("   Diameter: ", diameter)
-print("   Average Distance: ",avg_distance)
-print("OPTION B:")
-print("   Average Clustering: ",avg_clustering)
-print("   Transitivity: ",transitivity)
-print("   Computed ransativity: ",computed_transitivity)
+# Compute transitivity using custom function
+transitivity_custom = custom_transitivity(G_main)
+
+print(f"Average Clustering (NetworkX): {avg_clustering_nx}")
+print(f"Average Clustering (Custom): {avg_clustering_custom}")
+print(f"Transitivity (NetworkX): {transitivity_nx}")
+print(f"Transitivity (Custom): {transitivity_custom}")
+
+# Visualization (Optional)
+nx.draw(G_main, with_labels=True)
+plt.show()
 
 
 #-----------------------------------------------------------WEEK 3
 '''
 QUERY 3:
-Depending on what seems more relevant in your graph, pick one of the following local notions
-- Decay centrality
+Depending on what seems more relevant in your graph of the following local notions
 - Betweeness centrality
 - Closeness centrality
-- Clustering
-- Any other notions that you invent
+
 1) Provide a code computing the given centrality using basic functions of networkx (you are not allowed to use directly nx."what you want").
 2) Discuss why you picked this measure and who is the most central in your network based on your choice.
 3) Provide the cumulative distribution for this centrality and give a graphical representation of your graph (log-log, log or normal representation as you think it is more relevant).
